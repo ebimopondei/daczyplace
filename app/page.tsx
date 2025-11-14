65dabadef8411c67e9e6cd4d8f501e3aa1aba3a2
Pondei Ebimobowei <ebi4jah15@gmail.com>
2025-11-14 12:47:22 +0100

    feat: create header section

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,73 @@
+"use client"
+import { MenuIcon } from "@/components/menu-icon";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const navigation = [
+    { name: 'Service', href: '#' },
+    { name: 'Apartments', href: '#' },
+    { name: 'About', href: '#' },
+    { name: 'Contact', href: '#' },
+    { name: 'Testimonial', href: '#' },
+  ]
+
+  const [open, setOpen] = useState(false);
+
+
+    
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div>
+      <div>
+        <div className="bg-[url('/images/hero.jpg')] bg-cover h-screen flex flex-col">
+          <div className="flex items-center justify-center md:py-10">
+            <header className="relative flex justify-between items-center p-4 bg-secondary/80 md:bg-secondary/70 basis-full  md:basis-10/12">
+              <Image
+                src={"/logo.png"}
+                alt="logo"
+                className="w-[182px] md:-[228px] h-6"
+                width={228}
+                height={30}
+              />
+
+              <MenuIcon open={open} setOpen={setOpen} />
+
+              {/* <div> */}
+                <div className=" hidden md:flex items-center justify-center text-background font-bold gap-10">
+                  <nav className="flex items-center gap-6">
+                    { navigation.map((item) => {
+                      return(
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className="uppercase"
+                        >{item.name}</a>
+                      )
+                    })}
+                  </nav>
+                  
+                  <div className="bg-primary text-dark text-lg font-bold py-4 px-10">
+                    <p>Book Now</p>
+                  </div>
+                </div>
+              {/* </div> */}
+    
+
+            </header>
+
+          </div>
+
+          <main>
+            <section className="flex items-end border">
+              <div className="basis-10/12">
+                <h1>Discover Your Next Stay With Style</h1>
+
+              </div>
+            </section>
+          </main>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+
     </div>
   );
 }
---
+++ components/menu-icon.tsx
@@ -0,0 +1,53 @@
+"use client"
+import { Dispatch, SetStateAction, useState } from "react";
+
+export function MenuIcon({ open, setOpen }: { open: boolean; setOpen: Dispatch<SetStateAction<boolean>> }) {
+  return (
+    <button
+          aria-label={open ? "Close navigation" : "Open navigation"}
+          aria-expanded={open}
+          onClick={() => setOpen((s) => !s)}
+          className={"group w-12 h-12 rounded-lg flex md:hidden items-center justify-center backdrop-blur-sm shadow-lg ring-1 ring-slate-900/5 hover:scale-105 transform transition duration-200"}
+        >
+          <svg
+            className="w-6 h-6 relative"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              className={`transform transition duration-300 ease-in-out stroke-slate-900 dark:stroke-white`} 
+              d="M4 7h16"
+              strokeWidth="2"
+              strokeLinecap="round"
+              style={{
+                transformOrigin: '50% 50%',
+                transform: open ? 'translateY(0rem) translateX(-0.3rem) rotate(45deg)' : 'none',
+                opacity: open ? 0.9 : 1,
+              }}
+            />
+            <path
+              className={`transform transition duration-300 ease-in-out stroke-slate-900 dark:stroke-white`} 
+              d="M4 12h16"
+              strokeWidth="2"
+              strokeLinecap="round"
+              style={{
+                transformOrigin: '50% 50%',
+                transform: open ? 'scaleX(0)' : 'none',
+                opacity: open ? 0 : 1,
+              }}
+            />
+            <path
+              className={`transform transition duration-300 ease-in-out stroke-slate-900 dark:stroke-white`} 
+              d="M4 17h16"
+              strokeWidth="2"
+              strokeLinecap="round"
+              style={{
+                transformOrigin: '50% 50%',
+                transform: open ? 'translateY(-0.3rem) translateX(-0.3rem) rotate(-45deg)' : 'none',
+                opacity: open ? 0.9 : 1,
+              }}
+            />
+          </svg>
+        </button>
+  );}
